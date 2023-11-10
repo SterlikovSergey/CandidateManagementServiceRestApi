8a14f9450b44a8f2955df7edb9f870838266d11a
St.Sergey.Minsky <Sterlikovsergey@icloud.com>
2023-11-10 06:17:46 +0300

    Create Entity Direction Test TestResult TestUser

---
+++ src/main/java/by/sterlikov/candidatemanagementservicerestapi/model/TestUser.java
@@ -0,0 +1,34 @@
+package by.sterlikov.candidatemanagementservicerestapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "test_candidate")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TestUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "candidate_id")
+    private User ownerTest;
+
+    @ManyToOne
+    @JoinColumn(name = "test_id")
+    private Test test;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "ownerResult")
+    private List<TestResult> results = new ArrayList<>();
+
+}
